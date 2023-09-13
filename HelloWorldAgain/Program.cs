f2da043defd9295c347ea0fbd2b903b122e3124b
Lauralynn Trainor <144450292+LauralynnT@users.noreply.github.com>
2023-09-13 14:26:56 -0300

    Added rank percentile

--- HelloWorldAgain/Program.cs
+++ HelloWorldAgain/Program.cs
@@ -18,17 +18,19 @@ namespace HelloWorldAgain
             int enemyValue;
             string name;
             string gamertag;
-            
+            float percentile;
 
             score = 0;
             enemyValue = 250;
             name = "Lauralynn";
             gamertag = "StupidPieTrap";
+            percentile = 80.48f;
 
             score+=enemyValue;
+            percentile = (92043f / 380972f)*100f;
 
             Console.WriteLine("Score: " + score);
-
+            Console.WriteLine("Percentile Rank: " + percentile + "%");
             Console.WriteLine(name + " " + gamertag);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey(true);
